2a11d4ff76b66087f434f93d1c3776fc0074449b
Elliot <ellthrowaway2@icloud.com>
2024-12-24 12:05:18 -0500

    feat: methods to generate a new deck, shuffle, and draw cards; additional method to get deck size for use in dealing cards to players

--- Deck.java
+++ Deck.java
@@ -1,5 +1,48 @@
 package week06Assignment;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
+
+	static List<Card> cards = new ArrayList<Card>();
+	private String[] cardNames = { "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack",
+			"Queen", "King", "Ace" };
+	private String[] suits = { "Hearts", "Spades", "Diamonds", "Clubs" };
+
+	// Constructor -- generate deck
+	public List<Card> generateNewDeck() {
+		// Generate cards of each suit
+		for (String suit : suits) {
+			// Generate cards of each name and value
+			for (int i = 0; i < 13; i++) {
+				Card cardToAdd = new Card(cardNames[i], suit, i + 2);
+				cards.add(cardToAdd);
+			}
+		}
+		return cards;
+	}
+	
+	public int getDeckSize() {
+		return cards.size();
+	}
+
+	// Shuffle method
+	public List<Card> shuffle() {
+		// Use built-in Collections shuffle method
+		Collections.shuffle(cards);
+		return cards;
+	}
+	
+	// Draw method
+	public Card draw() {
+		// Remove first card from deck
+		Card drawnCard = cards.remove(0);
+		if (cards.size() == 0) {
+			System.out.println("Error: No cards left to draw.");
+		}
+		return drawnCard;
+	}
 
 }
