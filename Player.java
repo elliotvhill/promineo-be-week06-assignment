7d9229701a91aa325502a359f022f998789c4416
Elliot <ellthrowaway2@icloud.com>
2024-12-24 12:06:39 -0500

    feat: getters and setters; methods to draw and add cards to player's hand, flip cards; placeholder method for incrementScore

--- Player.java
+++ Player.java
@@ -1,33 +1,59 @@
 package week06Assignment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
 
 	private String name;
-	private List<Card> hand;
+	List<Card> hand = new ArrayList<Card>();
 	private int score;
 	
-	// Constructor
+	// Initial constructor before player has drawn any cards
+	public Player(String name, int score) {
+		this.name = name;
+		this.score = 0;
+	}
+	
 	public Player(String name, List<Card> hand, int score) {
 		this.name = name;
 		this.hand = hand;
 		this.score = 0;
 	}
 	
-	public Player(String name, int score) {
-		this.name = name;
-		this.score = 0;
+	public int getScore() {
+		return score;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public List<Card> getHand() {
+		return hand;
 	}
 	
 	public void describe(){
+		// TODO: add hand to describe print statement
 		System.out.println(String.format("Player: %s\nScore: %d\nHand: ", name, score));
 	}
 	
-//	flip(){}
-//	
-//	draw(){}
-//	
-//	incrementScore(){}
+//	Draw a card and add to player's hand
+	public List<Card> draw(Deck deck){
+		hand.add(deck.draw());
+		return hand;
+	}
+	
+	// Remove and return the top card in the hand
+	public Card flip(List<Card> hand){
+		Card flippedCard = ((Deck) hand).draw();
+		hand.remove(flippedCard);
+		return flippedCard;
+	}
+	
+	// Increment player's score
+	public int incrementScore(){
+		return score++;
+	}
 	
 }
