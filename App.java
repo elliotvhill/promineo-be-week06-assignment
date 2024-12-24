f429a4ce4ade8425188b6cbe712093a630269f52
Elliot <ellthrowaway2@icloud.com>
2024-12-24 12:08:28 -0500

    feat: app generates a new deck and shuffles cards, then deals 26 cards to each player; removed test card code

--- App.java
+++ App.java
@@ -2,14 +2,30 @@ package week06Assignment;
 
 public class App {
 
+	static Deck deck = new Deck();
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		Card testCard = new Card("Two", "Hearts", 2);
-		testCard.describe();
 		
+		// Generate and shuffle new card deck
+		deck.generateNewDeck();
+		deck.shuffle();
+
+		// Create players
 		Player player1 = new Player("Geralt", 0);
 		player1.describe();
+		
+		Player player2 = new Player("Dandelion", 0);
+		player2.describe();
+
+		// Draw and deal cards to player1's hand
+		for (int i = 26; i > 0; i--) {
+			player1.draw(deck);
+		}
+		
+		// Deal remaining cards to player2's hand
+		while (deck.getDeckSize() >= 0) {
+			player2.hand.add(deck.draw());
+		}
 		
 	}
 
